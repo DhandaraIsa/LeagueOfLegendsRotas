645764fb40c9dc2431f1cef09bb4a51195293122
Dhandara <dhandara.isabela2001@gmail.com>
2025-09-12 22:28:21 -0300

    atualização

--- index.js
+++ index.js
@@ -18,17 +18,59 @@ async function getChampions() {
 
 async function callchampions(type) {
     const champions = await getChampions();
-    getRoute(type, champions).then(championsByType => {
-        console.log(`Campeões do tipo ${type}:`, championsByType);
-    });
+    const championsByType = await getRoute(type, champions);
+    listChampions(championsByType);
+    
+    // getRoute(type, champions).then(championsByType => {
+    //     console.log(`Campeões do tipo ${type}:`, championsByType);
+    // });
 }
 
 async function getRoute(type, champions) {
     const typechampions = champions.filter(champion => champion.roles.includes(type));
     return typechampions;
 }
-        
 
-    
+async function listChampions(champions) {
+    let card = $('.card-champions');
+    card.empty(); // Limpa os cards existentes
+    const url = 'https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/';
+
+    champions.forEach(champion => {
+        let cardHtml = `
+            <div class="card">
+                <img src="${url}${champion.id}.png" alt="${champion.name}">   
+                <div class="">
+                    <p>${champion.name}</p>
+                    <p>${champion.description}</p>
+                </div>
+            </div>
+        `;
+        card.append(cardHtml);
+        
+    });
+}
+
+ $('#searchInput').on('keyup', function() {
+    var valorInput = $(this).val();
+    var quantidadeCaracteres = valorInput.length;
+    $('#contador').text(quantidadeCaracteres);
+    if (quantidadeCaracteres >= 3) {
+        searchChampion(valorInput);
+    }
+  });
+
+  async function searchChampion(name) {
+    const champions = await getChampions();
+    const filteredChampions = filterChampionsByName(name, champions);
+    listChampions(filteredChampions);
+
+  }
+
+function filterChampionsByName(query, champions) {
+    return champions.filter(champion =>
+        champion.name.toLowerCase().includes(query.toLowerCase())
+    );
+}
 
         
